ec210e856ec6f79e073b804bd5eecf4a7383b18c
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-10-26 15:55:50 -0700

    Set up sample create-post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 import SignOutButton from "@/components/SignOutButton";
+import { CreatePost } from "@/components/create-post";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { getServerAuthSession } from "@/server/auth";
-import { api } from "@/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello.query({ text: "from tRPC" });
   const session = await getServerAuthSession();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1>{hello ? hello.greeting : "Loading tRPC query..."}</h1>
       <h2>{session && `You are signed in as ${session.user.name}`}</h2>
       <ModeToggle />
       <SignOutButton />
+      <CreatePost />
     </main>)
 }
