0e51b10f8a7dc6bfe6c483a4c1301ee0cb29ee6f
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-10-26 23:26:42 -0700

    Remove SignOutButton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import { Post } from '@/components/Post';
-import { SignOutButton } from '@/components/SignOutButton';
 import { api } from '@/trpc/server';
 
 export default async function Home() {
   const posts = await api.post.getPosts.query();
 
   return (
-    <main className="flex flex-col items-center justify-center px-8">
-      <SignOutButton />
+    <main className="flex flex-col items-center justify-center p-8">
       {posts.map((post) => (
         <Post key={post.id} post={post} user={post.createdBy} comments={post.comments} likes={post.likes} />
       ))}
